5a4c3473c3e5d0361b0c19b9038b1ec6dd1d184f
experionarjun <arjun.sreedhar@experionglobal.com>
2017-04-19 19:45:57 +0530

    states and props

---
+++ src/app/app.config.js
@@ -0,0 +1,2 @@
+//Youtube api key
+export const API_KEY = "AIzaSyAj_T96XTEfPc3jJwuRNWkaCwY2FBfC4a0"
---
+++ src/app/components/search.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import '../styles/main.css'
+
+export default class SearchBar extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      term : ""
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  render() {
+    return(
+      <div>
+        <input
+         placeholder = 'Search.....'
+         onChange={this.onInputChange}
+         className='searchInput'
+        />
+      </div>
+    )
+  }
+
+
+   onInputChange(e){
+    let temp = e.target.value;
+    console.log(temp)
+    this.setState({ term: temp});
+  }
+
+}
